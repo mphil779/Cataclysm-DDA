607e2d619ccf08ac880e06795e65c31507711b8a
MPhil <mphil779@gmail.com>
2025-03-23 12:15:45 -0400

    Remove unused index variable in worldfactory.cpp

--- src/worldfactory.cpp
+++ src/worldfactory.cpp
@@ -614,7 +614,6 @@ void worldfactory::draw_mod_list( const catacurses::window &w, int &start, size_
         const int wwidth = getmaxx( w ) - 1 - 3; // border (1) + ">> " (3)
 
         unsigned int iNum = 0;
-        int index = 0;
         bool bKeepIter = false;
         int iCatBeforeCursor = 0;
 
@@ -634,7 +633,7 @@ void worldfactory::draw_mod_list( const catacurses::window &w, int &start, size_
         }
 
         int larger = ( iMaxRows > static_cast<int>( iModNum ) ) ? static_cast<int>( iModNum ) : iMaxRows;
-        for( auto iter = mods.begin(); iter != mods.end(); ++index ) {
+        for( auto iter = mods.begin(); iter != mods.end(); ) {
             if( iNum >= static_cast<size_t>( start ) && iNum < static_cast<size_t>( start + larger ) ) {
                 if( !mSortCategory[iNum].empty() ) {
                     bKeepIter = true;
